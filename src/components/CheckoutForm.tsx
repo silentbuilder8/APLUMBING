34f95a1fe3302268f7ae476c48a38b772c86c6e8
silentbuilder8 <pierrebuilder1@gmail.com>
2025-06-08 04:55:07 +0100

    Update CheckoutForm.tsx

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from './Button';
 
-const CALENDLY_URL = 'https://calendly.com/ai-discoveryy/unlock-the-power-of-ai-for-your-business';
+const CALENDLY_URL = 'https://www.paypal.com/ncp/payment/EFQ6F4YNYZ7RA';
 
 const CheckoutForm: React.FC = () => {
   const [formData, setFormData] = useState({
