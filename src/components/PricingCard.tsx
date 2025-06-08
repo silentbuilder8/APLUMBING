8eb49cf7d8f19e9c04e581d6bed65df917bb5aef
silentbuilder8 <pierrebuilder1@gmail.com>
2025-06-08 05:10:04 +0100

    Update PricingCard.tsx

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './Button';
 
-const CALENDLY_URL = 'https://www.paypal.com/ncp/payment/EHHR76BTJKTMS';
+const CALENDLY_URL = 'https://www.paypal.com/ncp/payment/EFQ6F4YNYZ7RA';
 
 type PricingCardProps = {
   title: string;
